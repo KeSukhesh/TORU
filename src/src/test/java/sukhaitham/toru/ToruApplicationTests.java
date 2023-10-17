370cb3fa1e17458f5a61566ad7e51cab0e4ed7f3
Sukhesh <sukheshpatro@gmail.com>
2023-10-18 02:39:06 +1100

    feat: Installed the relevant Spring Boot dependency

---
+++ src/src/test/java/sukhaitham/toru/ToruApplicationTests.java
@@ -0,0 +1,13 @@
+package sukhaitham.toru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
