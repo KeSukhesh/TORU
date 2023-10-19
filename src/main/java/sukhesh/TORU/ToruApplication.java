52e0314780f8b4b2a6a7fea418274f89c7d63c5d
Sukhesh <sukheshpatro@gmail.com>
2023-10-19 23:06:16 +1100

    feat: tracking everthing in the repo

---
+++ src/main/java/sukhesh/TORU/ToruApplication.java
@@ -0,0 +1,13 @@
+package sukhesh.TORU;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToruApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sukhesh/TORU/ToruApplicationTests.java
@@ -0,0 +1,13 @@
+package sukhesh.TORU;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
