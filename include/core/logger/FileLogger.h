aa24f6d524dafc29aa5e15375a5a4f89c754de4a
Sukhesh Patro <sukheshPatro@student.unsw.edu.au>
2024-08-28 03:58:19 +1000

    feat: IMplemented Custom Logger with different log levels

---
+++ include/core/logger/FileLogger.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "Logger.h"
+
+class FileLogger : public Logger
+{
+public:
+    FileLogger(const std::string& filename) : logFile_(filename, std::ios::app) {
+        if (!logFile_.is_open()) {
+            throw std::runtime_error("Unable to open log file");
+        }
+    }
+
+    ~FileLogger() {
+        if (logFile_.is_open()) {
+            logFile_.close();
+        }
+    }
+
+    void setLogFile(const std::string& filename) {
+        logFile_.open(filename, std::ios::app);
+    }
+
+protected:
+    void log(LogLevel level, const std::string& message) override {
+        if (level >= getLogLevel()) {
+            std::lock_guard<std::mutex> lock(logMutex);
+            std::string formattedMessage = formatMessage(level, message);
+            std::cout << formattedMessage; // Console logging
+            if (logFile_.is_open()) {
+                logFile_ << formattedMessage; // File logging
+            }
+
+        }
+    }
+
+private:
+    std::ofstream logFile_;
+    std::mutex logMutex;
+};
---
+++ include/core/logger/Logger.h
@@ -0,0 +1,89 @@
+#pragma once
+
+#include <string>
+#include <iostream>
+#include <exception>
+#include <fstream>
+#include <sstream>
+#include <mutex>
+#include <memory>
+#include <chrono>
+#include <ctime>
+#include <map>
+
+
+#include "LogLevel.h"
+
+class Logger
+{
+public:
+    virtual ~Logger() = default;
+
+    void setLogLevel(LogLevel level) {
+        logLevel_ = level;
+    }
+
+    LogLevel getLogLevel() {
+        return logLevel_;
+    }
+
+    void logInfo(const std::string& message) {
+        log(LogLevel::INFO, message);
+    }
+
+    void logWarning(const std::string& message) {
+        log(LogLevel::WARNING, message);
+    }
+
+    void logError(const std::string& message) {
+        log(LogLevel::ERROR, message);
+    }
+
+    void logDebug(const std::string& message) {
+        log(LogLevel::DEBUG, message);
+    }
+
+    void logInfo(const std::string& module, const std::exception& ex) {
+        log(LogLevel::INFO, module);
+        throw ex;
+    }
+
+    void Logger::logWarning(const std::string& module, const std::exception& ex) {
+        log(LogLevel::WARNING, module);
+        throw ex;
+    }
+
+    void Logger::logError(const std::string& module, const std::exception& ex) {
+        log(LogLevel::ERROR, module);
+        throw ex;
+    }
+
+protected:
+    std::string formatMessage(LogLevel level, const std::string& message) {
+        std::ostringstream oss;
+        oss << "[" << getCurrentTime() << "] [" << logLevelToString(level) << "] " << message << std::endl;
+        return oss.str();
+    }
+
+private:
+    virtual void log(LogLevel level, const std::string& message) = 0;
+    LogLevel logLevel_ = LogLevel::INFO;
+
+    std::string getCurrentTime() {
+        auto now = std::chrono::system_clock::now();
+        std::time_t now_time = std::chrono::system_clock::to_time_t(now);
+        char buffer[20];
+        std::strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S", std::localtime(&now_time));
+        return std::string(buffer);
+    }
+
+    std::string logLevelToString(LogLevel level) {
+        static std::map<LogLevel, std::string> logLevelMap = {
+            { LogLevel::INFO, "INFO" },
+            { LogLevel::WARNING, "WARNING" },
+            { LogLevel::ERROR, "ERROR" },
+            { LogLevel::DEBUG, "DEBUG" }
+        };
+        return logLevelMap[level];
+    }
+};
