62b7366c9e56ce2b66bca8b6bf08811300d85d92
Sukhesh Patro <sukheshPatro@student.unsw.edu.au>
2024-08-30 19:14:00 +1000

    fix: Fixed LSP violation in handle_server_connection() in BackgroundService class

--- src/background_service/BackgroundService.cpp
+++ src/background_service/BackgroundService.cpp
@@ -18,7 +18,31 @@ void BackgroundService::start_server() {
 
 // base method to get overriden
 void BackgroundService::handle_server_connection(tcp::socket socket) {
-    return;
+    std::stringstream ss;
+    // can probably log here for indepedent workers.
+
+    try {
+        boost::asio::streambuf buffer;
+        std::istream input_stream(&buffer);
+        boost::asio::read_until(socket, buffer, "\r\n");
+        std::string request_line;
+        std::getline(input_stream, request_line);
+        if (!request_line.empty() && request_line.back() == '\r') {
+            request_line.pop_back();
+        }
+
+        std::string status_line = "HTTP/1.1 200 OK";
+        std::string filename = "../src/util/hello.html";
+
+        std::string contents = read_file_to_string(filename);
+        std::string length = std::to_string(contents.size());
+        std::string response = status_line + "\r\nContent-Length: " + length + "\r\n\r\n" + contents;
+        boost::asio::write(socket, boost::asio::buffer(response));
+    }
+    catch (std::exception& e) {
+        // log
+        // std::cerr << "Error: " << e.what() << std::endl;
+    }
 }
 
 std::string BackgroundService::read_file_to_string(const std::string& filename) {
